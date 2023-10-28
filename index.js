c69aa9964edf895cee2cabcb8d9ca221dc46076a
Tarun Devgan <107904243+devgnTarun@users.noreply.github.com>
2023-10-28 16:16:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const imapConfig = [{
 },
 {
     user: "augierthomas.audrey@outlook.com",
-    password: "@Zerty2016",
+    password: "@-----",
     host: "imap-mail.outlook.com",
     port: 993,
     tls: true
